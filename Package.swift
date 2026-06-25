8888b514663e67bf51c7ec5dfb1e5526388c7ba8
Hareem Adderley <hadderley@kingpinapps.com>
2026-06-25 10:58:05 -0500

    chore: require swift-cardano-utils 0.5.3 for cardano-cli 11.0 pool-state decode

--- Package.swift
+++ Package.swift
@@ -31,7 +31,7 @@ let package = Package(
         .package(url: "https://github.com/Kingpin-Apps/swift-cardano-txbuilder.git", from: "1.0.2"),
         .package(url: "https://github.com/Kingpin-Apps/swift-cardano-txvalidator.git", from: "0.2.2"),
         .package(url: "https://github.com/Kingpin-Apps/swift-cardano-signer.git", from: "0.1.2"),
-        .package(url: "https://github.com/Kingpin-Apps/swift-cardano-utils.git", from: "0.5.2"),
+        .package(url: "https://github.com/Kingpin-Apps/swift-cardano-utils.git", from: "0.5.3"),
         .package(url: "https://github.com/Kingpin-Apps/swift-koios.git", from: "0.2.0"),
         .package(url: "https://github.com/Kingpin-Apps/swift-cardano-wallet.git", from: "1.1.2"),
         .package(url: "https://github.com/Kingpin-Apps/swift-handles-api.git", from: "0.1.1"),
