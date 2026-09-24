d6887ab9e0466cdf63b2fb500184ff57107d2cad
Hareem Adderley <hadderley@kingpinapps.com>
2026-09-24 17:13:04 -0500

    fix: pick up the tag-258 indefinite-array decode fix from core 0.7.1 and txvalidator 0.3.4

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-crypto.git", from: "4.5.0"),
         .package(url: "https://github.com/apple/swift-system.git", from: "1.6.3"),
         .package(url: "https://github.com/jpsim/Yams.git", from: "5.4.0"),
-        .package(url: "https://github.com/Kingpin-Apps/swift-cardano-core.git", from: "0.7.0"),
+        .package(url: "https://github.com/Kingpin-Apps/swift-cardano-core.git", from: "0.7.1"),
         .package(url: "https://github.com/Kingpin-Apps/swift-cbor-codable.git", .upToNextMinor(from: "0.3.2")),
         .package(
             url: "https://github.com/Kingpin-Apps/swift-cardano-chain.git",
@@ -35,7 +35,7 @@ let package = Package(
         .package(url: "https://github.com/Kingpin-Apps/swift-cardano-cips.git", from: "0.3.8"),
         .package(url: "https://github.com/Kingpin-Apps/swift-cardano-token-registry.git", from: "0.2.0"),
         .package(url: "https://github.com/Kingpin-Apps/swift-cardano-txbuilder.git", from: "1.0.5"),
-        .package(url: "https://github.com/Kingpin-Apps/swift-cardano-txvalidator.git", from: "0.3.3"),
+        .package(url: "https://github.com/Kingpin-Apps/swift-cardano-txvalidator.git", from: "0.3.4"),
         .package(url: "https://github.com/Kingpin-Apps/swift-cardano-signer.git", from: "0.1.2"),
         .package(url: "https://github.com/Kingpin-Apps/swift-cardano-utils.git", from: "0.5.7"),
         .package(url: "https://github.com/Kingpin-Apps/swift-koios.git", from: "0.2.2"),
